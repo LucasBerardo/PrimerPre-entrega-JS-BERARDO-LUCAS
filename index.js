7b4b4a199a46c5afed0e875feab3e947cfcb9b1b
Lucas Berardo <lucasberardo2@gmail.com>
2023-06-19 14:29:09 -0300

    segunda-pre entrega

--- index.js
+++ index.js
@@ -1,118 +1,190 @@
 // constantes a utilizar
-const opcion_salir = 4;
+// const opcion_salir = 4;
 const ArrayProductos = []
+// const ArrayCarrito = [];
+// const ArrayPromociones = [];
+let codigoProductos = 0
+
 
 //productos
 
 class Producto {
-    constructor (nombre, precio,categoria, cantidad ){
-    // this.codigo
+    constructor (codigo,nombre, precio,categoria, cantidad,stock ){
+    this.codigo = codigo;
     this.nombre = nombre;
     this.precio = precio;
     this.categoria = categoria;
     this.cantidad = cantidad;
+    this.stock = stock
     }
 }
 
-// creamos los productos y los incorporamos al array
-const incorporarProductos = () => {
-const nueces = new Producto ("Nueces", 1850, "Frutos Secos", "por 1kg" )
-ArrayProductos.push (nueces)
 
-const almendras = new Producto ("Almendras", 4800, "Frutos Secos", "por 1kg" )
-ArrayProductos.push (almendras)
+// creamos los productos y promociones, luego los incorporamos al array
 
-const pistachos = new Producto ("Pistachos", 4950, "Frutos Secos", "por 1kg")
-ArrayProductos.push (nueces)
+const nueces = new Producto (codigoProductos++, "Nueces", 1850, "Frutos Secos", "por 1kg", 10 );
+ArrayProductos.push (nueces);
 
-const anana = new Producto ("Anana", 1500, "Frutas Abrillantadas", "por 1kg")
-ArrayProductos.push (anana)
+const almendras = new Producto (codigoProductos++,"Almendras", 4800, "Frutos Secos", "por 1kg", 20 );
+ArrayProductos.push (almendras);
 
-const ciruelas = new Producto ("Ciruelas", 1700, "Frutas Abrillantadas", "por 1kg")
-ArrayProductos.push (ciruelas)
+const pistachos = new Producto (codigoProductos++, "Pistachos", 4950, "Frutos Secos", "por 1kg", 30);
+ArrayProductos.push (pistachos);
 
-const duraznos = new Producto ("duraznos", 2100, "Frutas Abrillantadas", "por 1kg")
-ArrayProductos.push (duraznos)
+const anana = new Producto (codigoProductos++, "Anana", 1500, "Frutas Abrillantadas", "por 1kg", 40);
+ArrayProductos.push (anana);
 
+const ciruelas = new Producto (codigoProductos++, "Ciruelas", 1700, "Frutas Abrillantadas", "por 1kg", 45);
+ArrayProductos.push (ciruelas);
 
-const oregano = new Producto ("Oregano", 600, "Condimentos", "por 500g")
-ArrayProductos.push (oregano)
-
-const ajiMolido = new Producto ("Aji Molido", 600, "Condimentos", "por 500g")
-ArrayProductos.push (ajiMolido)
-
-const ajoEnPolvo = new Producto ("Ajo En Polvo", 600, "Condimentos", "por 500g")
-ArrayProductos.push (ajoEnPolvo)
-}
-
-incorporarProductos();
+const duraznos = new Producto (codigoProductos++, "duraznos", 2100, "Frutas Abrillantadas", "por 1kg", 50);
+ArrayProductos.push (duraznos);
 
 
+const oregano = new Producto (codigoProductos++, "Oregano", 600, "Condimentos", "por 500g", 60);
+ArrayProductos.push (oregano);
 
+const ajiMolido = new Producto (codigoProductos++, "Aji Molido", 600, "Condimentos", "por 500g", 55);
+ArrayProductos.push (ajiMolido);
+
+const ajoEnPolvo = new Producto (codigoProductos++, "Ajo En Polvo", 600, "Condimentos", "por 500g", 150) ;
+ArrayProductos.push (ajoEnPolvo);
+
+const promo1 = new Producto (codigoProductos++, "nueces y almendras", 6000, "promo", "por 2kg", 15);
+ArrayProductos.push (promo1);
+
+const promo2 = new Producto (codigoProductos++, "ciruelas y durazno", 3200, "promo", "2kg", 250);
+ArrayProductos.push (promo2);
+
+const promo3 = new Producto (codigoProductos++, "oregano y ajo", 1000, "promo", "por 1kg", 500) ;
+ArrayProductos.push (promo3);
+
+// const mostrarProductoPorCategoria = (categoria) =>{
+//         const filtrado = ArrayProductos.filter((el)=> el.categoria === categoria); 
+//         let mensajeAMostrar = '';
+//         filtrado.forEach((el) =>{
+//                 mensajeAMostrar += "\n" + el.codigo + "-Producto: " + el.nombre + " " + "\nPrecio: " + el.precio
+//         });
+//         const codigo = parseInt(prompt(mensajeAMostrar + "\n" + "Ingrese producto para agregar al carrito"));
+//         const productoEcontrado = ArrayProductos.find(el => el.codigo === codigo);
+//         ArrayCarrito.push(productoEcontrado);
+//     }
+    
 
 // Creamos las categorias a utilizar
 
-const verProductos = () => {
-    let opciones;
-    opciones = parseInt(prompt ("Elige el producto que desea \n 1-Futos secos \n 2- Fruntas abrillantadas \n 3-Condimentos \n 4- Volver al menu anterior"))
-    while (opciones != opcion_salir){
 
-        switch (opciones){
-    
-            case 1:
-                verFrutosSecos()
-                break
-    
-    
-            case 2:
-                verFrutasAbrillantadas()
-                break
-    
-            case 3:
-                verCondimentos()
-                break
-    
-            case 4:
-                break
-    
-                default: alert("La opcion no es valida")
-    
-        }
-        opciones = parseInt(prompt ("Elige el producto que desea \n 1-Nueces \n 2- Almendras \n 3-Pistachos \n 4- Volver al menu anterior"))
+// const verProductos = () => {
+//     let opcion ;
+//     opcion = parseInt(prompt("Elige el producto que desea \n 1-Frutos Secos \n 2- Frutas Abrillantadas \n 3-Condimentos \n 4- Volver al menu anterior"));
+//     while(opcion != opcion_salir){
+//         switch(opcion){
+//             case 1: 
+//                     mostrarProductoPorCategoria("Frutos Secos");
+//                     break;
+//             case 2: mostrarProductoPorCategoria("Frutas Abrillantadas");
+//                     break;
+//             case 3: mostrarProductoPorCategoria("Condimentos");
+//                     break;
+//             default:
+//                 alert("Ingreso una opcion invalida.");
+//                 break;
+//         }
+//         opcion = parseInt(prompt("Elige el producto que desea \n 1-Frutos Secos \n 2- Frutas Abrillantadas \n 3-Condimentos \n 4- Volver al menu anterior"));
+//     }
+// }
+// //creamos las opciones de promociones
+
+// const verPromociones = () => {
+//     let opcion ;
+//     opcion = parseInt(prompt("Elige la promocion que desea \n 1-nuces y almedras \n 2- ciruelas y durazno \n 3-oregano y ajo \n 4- Volver al menu anterior"));
+//     while(opcion != opcion_salir){
+//         switch(opcion){
+//             case 1: 
+//                     mostrarPromo("nuces y almedras");
+//                     break;
+//             case 2: mostrarPromo("ciruelas y durazno");
+//                     break;
+//             case 3: mostrarPromo("oregano y ajo");
+//                     break;
+//             default:
+//                 alert("Ingreso una opcion invalida.");
+//                 break;
+//         }
+//         opcion = parseInt(prompt("Elige el producto que desea \n 1-Frutos Secos \n 2- Frutas Abrillantadas \n 3-Condimentos \n 4- Volver al menu anterior"));
+//     }
+// }
+
+// // const mostrarPromo = (categoria) =>{
+// //         const filtrado = ArrayPromociones.filter((el)=> el.categoria === categoria); 
+// //         let promoAMostrar = '';
+// //         filtrado.forEach((el) =>{
+// //         promoAMostrar += "\n" + el.codigo + "-Promociones: " + el.nombre + " " + "\nPrecio: " + el.precio
+// //         });
+// //         const codigo = parseInt(prompt( promoAMostrar + "\n" + "Ingrese promo para agregar al carrito"));
+// //         const promoEcontrada = ArrayPromociones.find(el => el.codigo === codigo);
+// //         ArrayCarrito.push(promoEcontrada);
+// //     }
+
+// //Creamos el carrito
+
+// // const verCarrito = () => {
+// //     let mensajeAMostrar = 'Lista de procutos y/o promociones \n';
+// //     ArrayCarrito.forEach((el) =>{
+// //     mensajeAMostrar += "\n" + "Producto: " + el.nombre + " " + "\nPrecio: " + el.precio
+// //     });
+// //     const total = ArrayCarrito.reduce((acumulador,element) => acumulador + element.precio,0)
+// //     mensajeAMostrar += "\nEl total es: " + total
+// //     alert(mensajeAMostrar);
+// // }
+
+// // creamos el menu principal
+
+// let opcion = parseInt(prompt("Elige la opcion que desea \n 1-productos \n 2- Promociones \n 3-Carrito de compras \n 4- Salir"))
+
+// while(opcion != opcion_salir ){
+
+//     switch(opcion){
+//         case 1: 
+//                 verProductos();
+//                 break;
+//         case 2: 
+//         verPromociones();
+//                 break;
+//         case 3: 
+//         verCarrito();
+//                 break;
+//         default:
+//             alert("Ingreso una opcion invalida.");
+//             break;
+//     }
+
+//     opcion = parseInt(prompt("Elige la opcion que desea \n 1-productos \n 2- Combos disponibles \n 3-Carrito de compras \n 4- Salir"));
+// }
+
+// alert("Gracias vuelva pronto.")
+
+
+
+const pedido = ArrayProductos.map((el) =>{
+
+if (el.stock < 25){
+        return{
+               codigo: el.codigo,
+               nombre: el.nombre,
+                categoria: el.categoria,
+                stock: el.stock,
+                almacenamiento: "hay que pedir mas mercaderia" }
 }
+else {
+        return {
+               codigo: el.codigo,
+               nombre: el.nombre,
+                categoria: el.categoria,
+                stock: el.stock,
+                almacenamiento: "el sotck de este producto esta bien"}
 }
 
-// Creamos el menu principal
+})
 
-let opciones = prompt ("Elige la opcion que desea \n 1-productos \n 2- Combos disponibles \n 3-Carrito de compras \n 4- Salir")
-
-while (opciones != opcion_salir){
-
-    switch (opciones){
-
-        case 1:
-            verProductos()
-            break
-
-
-        case 2:
-            verPromociones()
-            break
-
-        case 3:
-            verCarrito()
-            break
-
-        case 4:
-            break
-
-            default: alert("Muchas gracias, lo esperamos pronto")
-
-    }
-
-    opciones = parseInt(prompt ("Elige la opcion que desea \n 1-productos \n 2- Combos disponibles \n 3-Carrito de compras \n 4- Salir"))
-
-}
-
-alert ("gracias por su compra, lo esperamos pronto")
-
+console.log (pedido)
